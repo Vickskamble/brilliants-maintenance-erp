3bf96bbb2ef262e4c806b0130d8a657dae4a2d2c
vickskamble <vikaskamble1324@gmail.com>
2026-09-25 12:39:57 +0530

    chore(hosting): static kiosk PWA for GitHub Pages (kiosk-site)

---
+++ kiosk-site/config.js
@@ -0,0 +1,6 @@
+window.KIOSK_CONFIG = {
+  SUPABASE_URL: "https://ekjakdhxodugncdpwkrj.supabase.co",
+  SUPABASE_ANON_KEY:
+    "sb_publishable_wfkvHgTcFlVdWOqbGRfpOA_O2pGf7YS",
+  QR_PREFIX: "BEM:",
+};
